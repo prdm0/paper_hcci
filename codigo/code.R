6ffa601431ef52157f5b5c7b0b726789d264e248
Pedro Rafael <pedro.rafael.marinho@gmail.com>
2020-02-20 15:21:31 -0300

    update

--- codigo/code.R
+++ codigo/code.R
@@ -1,14 +1,3 @@
-# y <- c(4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14)
-# x <- c(4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69)
-# result <- lm(ctl ~ trt)
-# X <- cbind(1, trt)
-# matrix_qr <- qr(X)
-# R <- qr.R(matrix_qr)
-# Q <- qr.Q(matrix_qr)
-# as.vector(solve(R)%*%t(Q)%*%y) # Estimadores.
-
-library(data.table)
-
 data("marketing", package = "datarium")
 head(marketing, 4)
 formula <-
@@ -24,8 +13,6 @@ hc <- function(formula, hc = 0L, k = 0.7) {
   data <- formula$model
   
   X <- as.matrix(cbind(intercept = 1, data[,-1]))
-  #y <- as.matrix(data[, 1])
-  
   
   result_qr <- qr(X, LAPACK = TRUE)
   
@@ -73,7 +60,7 @@ hc <- function(formula, hc = 0L, k = 0.7) {
 
 microbenchmark::microbenchmark(
   hcci::HC(formula, method = 0),
-  hc(formula = formula, hc = 5),
+  hc(formula = formula, hc = 0),
   times = 1e3L
 )
 
