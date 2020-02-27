ea3e7fa766f6d47da3ce6595a5fe71f1d2ebac9c
Pedro Rafael <pedro.rafael.marinho@gmail.com>
2020-02-27 17:06:03 -0300

    Update.

--- codigo/code.R
+++ codigo/code.R
@@ -4,56 +4,67 @@ formula <-
   lm(sales ~ youtube + facebook + newspaper, data = marketing)
 summary(formula)
 
+
+# Function to generate matrix X (matrix of independent variables) ---------
+
 create_x <- function(formula) {
   data <- formula$model
-  as.matrix(cbind(intercept = 1, data[,-1]))
+  as.matrix(cbind(intercept = 1, data[ ,-1]))
 }
 
-hc <- function(formula, hc = 0L, k = 0.7) {
-  if (class(formula) != "lm")
-    stop("\'formula\' must be an object of the \'lm\' class.")
-  
-  X <- create_x(formula = formula)
-  
-  result_qr <- qr(X, LAPACK = TRUE)
-  
-  R <- qr.R(result_qr)
-  Q <- qr.Q(result_qr)
-  
-  invr <- solve(R)
-  
-  h <- hatvalues(formula)
-  
+create_y <- function(formula) {
+  data <- formula$model
+}
+
+# Constant information ----------------------------------------------------
+X <- create_x(formula = formula)
+y <- marketing
+result_qr <- qr(X, LAPACK = TRUE)
+R <- qr.R(result_qr)
+Q <- qr.Q(result_qr)
+H <- X %*% solve(t(X) %*% X) %*% t(X)
+invr <- solve(R)
+
+olse <- function(X, y) 
+  solve(R) %*%  t(Q) %*% y
+
+residuals <- function(H, y) 
+  as.vector((diag(1, nrow = nrow(H), ncol = nrow(H)) - H) %*% y) 
+    
+hc <- function(y, X, H, hc = 0L, k = 0.7) {
   hc <- as.character(hc)
+  r <- residuals(H = H, y = y) 
+  h <- diag(H)
+  
   switch (hc,
           "0" = {
-            omega <- diag(formula$residuals ^ 2)
+            omega <- diag(r ^ 2)
             bread <- tcrossprod(invr, Q)
             result <- bread %*% tcrossprod(omega, bread)
             return(result)
           },
           "2" = {
-            omega <- diag(formula$residuals ^ 2 / (1 - h))
+            omega <- diag(r ^ 2 / (1 - h))
             bread <- tcrossprod(invr, Q)
             result <- bread %*% tcrossprod(omega, bread)
             return(result)
           },
           "3" = {
-            omega <- diag(formula$residuals ^ 2 / (1 - h) ^ 2)
+            omega <- diag(r ^ 2 / (1 - h) ^ 2)
             bread <- tcrossprod(invr, Q)
             result <- bread %*% tcrossprod(omega, bread)
             return(result)
           },
           "4" = {
             delta <- pmin(4, h / mean(h))
-            omega <- diag(formula$residuals ^ 2 / (1 - h) ^ delta)
+            omega <- diag(r ^ 2 / (1 - h) ^ delta)
             bread <- tcrossprod(invr, Q)
             result <- bread %*% tcrossprod(omega, bread)
             return(result)
           },
           "5" = {
             alpha <- pmin(h / mean(h), max(4, k * max(h) / mean(h)))
-            omega <- diag(formula$residuals ^ 2 / sqrt((1 - h) ^ alpha))
+            omega <- diag(r ^ 2 / sqrt((1 - h) ^ alpha))
             bread <- tcrossprod(invr, Q)
             result <- bread %*% tcrossprod(omega, bread)
             return(result)
@@ -70,15 +81,17 @@ t_value <- function(n = 1L, normal = FALSE) {
 }
 
 
-sample_boot <- function(formula, X, H, error){
+sample_boot <- function(formula){
   
-    
-  X %*% formula$coefficients
+  X <- create_x(formula)
+  
+  y <-  X %*% formula$coefficients * t_value(n = dim(X)[1L]) * formula$residuals / sqrt(1 - h)
+  y
 }
 
 microbenchmark::microbenchmark(
-  hcci::HC(formula, method = 5),
-  hc(formula = formula, hc = 5),
+    hcci::HC(formula, method = 5),
+  hc(y = y, X = X, H = H, hc = 5),
   times = 1e3L
 )
 
