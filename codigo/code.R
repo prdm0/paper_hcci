4950afefd21e405f64a3ce3df8606311511776ff
Pedro Rafael <pedro.rafael.marinho@gmail.com>
2020-02-27 15:17:55 -0300

    create t_value function.

--- codigo/code.R
+++ codigo/code.R
@@ -4,15 +4,16 @@ formula <-
   lm(sales ~ youtube + facebook + newspaper, data = marketing)
 summary(formula)
 
-
+create_x <- function(formula) {
+  data <- formula$model
+  as.matrix(cbind(intercept = 1, data[,-1]))
+}
 
 hc <- function(formula, hc = 0L, k = 0.7) {
   if (class(formula) != "lm")
     stop("\'formula\' must be an object of the \'lm\' class.")
   
-  data <- formula$model
-  
-  X <- as.matrix(cbind(intercept = 1, data[,-1]))
+  X <- create_x(formula = formula)
   
   result_qr <- qr(X, LAPACK = TRUE)
   
@@ -58,6 +59,21 @@ hc <- function(formula, hc = 0L, k = 0.7) {
             return(result)
           }
   )
+}
+
+t_value <- function(n = 1L, normal = FALSE) {
+  if (!normal) {
+    sample(x = c(-1, 1), size = n, replace = TRUE, prob = c(0.5, 0.5))
+  } else {
+    rnorm(n = n, mean = 0, sd = 1)
+  }
+}
+
+
+sample_boot <- function(formula, X, H, error){
+  
+    
+  X %*% formula$coefficients
 }
 
 microbenchmark::microbenchmark(
